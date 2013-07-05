25eca8b8551de1c34390b2a376f4e9b84007595c
Mitesh Patel <developermitesh@gmail.com>
2013-07-05 22:47:06 +0530

    modified artiplaq mounted photos price calculation based on retail price list

--- artiplaq_db/artiplaq_cache.sql
+++ artiplaq_db/artiplaq_cache.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 21, 2013 at 05:33 PM
+-- Generation Time: Jul 05, 2013 at 05:07 PM
 -- Server version: 5.5.20
 -- PHP Version: 5.3.10
 
@@ -411,6 +411,31 @@ CREATE TABLE IF NOT EXISTS `cache_views_data` (
   PRIMARY KEY (`cid`),
   KEY `expire` (`expire`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Cache table for views to store pre-rendered queries,...';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `watchdog`
+--
+
+DROP TABLE IF EXISTS `watchdog`;
+CREATE TABLE IF NOT EXISTS `watchdog` (
+  `wid` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key: Unique watchdog event ID.',
+  `uid` int(11) NOT NULL DEFAULT '0' COMMENT 'The users.uid of the user who triggered the event.',
+  `type` varchar(64) NOT NULL DEFAULT '' COMMENT 'Type of log message, for example "user" or "page not found."',
+  `message` longtext NOT NULL COMMENT 'Text of log message to be passed into the t() function.',
+  `variables` longblob NOT NULL COMMENT 'Serialized array of variables that match the message string and that is passed into the t() function.',
+  `severity` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT 'The severity level of the event; ranges from 0 (Emergency) to 7 (Debug)',
+  `link` varchar(255) DEFAULT '' COMMENT 'Link to view the result of the event.',
+  `location` text NOT NULL COMMENT 'URL of the origin of the event.',
+  `referer` text COMMENT 'URL of referring page.',
+  `hostname` varchar(128) NOT NULL DEFAULT '' COMMENT 'Hostname of the user who triggered the event.',
+  `timestamp` int(11) NOT NULL DEFAULT '0' COMMENT 'Unix timestamp of when event occurred.',
+  PRIMARY KEY (`wid`),
+  KEY `type` (`type`),
+  KEY `uid` (`uid`),
+  KEY `severity` (`severity`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table that contains logs of all system events.' AUTO_INCREMENT=7013 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- sites/all/themes/artiplaq/js/custom.js
+++ sites/all/themes/artiplaq/js/custom.js
@@ -7,11 +7,26 @@
         $('#mounted-photos-image-preview').html(imagepreview);
       }
       
-      
       $('#naticalchartzoon').addpowerzoom({
         distanceFromTheTop: 0,
         magnifiersize: [345, 240],
       });
+      
+      $('select[name^="line_item_fields[field_united_inch]"]').bind('change',function(){
+          
+          var w = $("#field-hidden-image .field-name-field-image").find('img').width();
+          var h = $("#field-hidden-image  .field-name-field-image").find('img').height();
+          var u = $(this).find('option:selected').text();
+           
+          var width = w * u / (w + h);
+          width =  Math.round(width * 100) / 100; 
+          var height =  h * u / (w + h);
+          height =  Math.round(height * 100) / 100;
+          $('#united-inch-size').html(width + '" X ' + height + '"');  
+          $('input[name^="line_item_fields[field_print_size]"]').val(width + '" X ' + height + '"');
+           
+      });
+      
     }
   };
 })(jQuery);
